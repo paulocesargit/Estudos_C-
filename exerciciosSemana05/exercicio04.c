a4360c1ab045fa8aa2314c3f3d418c0c6a0b40b1
Paulo Cesar <149436023+paulocesargit@users.noreply.github.com>
2026-04-22 19:24:18 -0300

    Atualizar o exercicio04.c

--- exerciciosSemana05/exercicio04.c
+++ exerciciosSemana05/exercicio04.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int num, soma = 0;
+
+    printf("Digite numeros (0 para parar): ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        soma += num;
+        scanf("%d", &num);
+    }
+
+    printf("Soma = %d\n", soma);
 
     return 0;
 }
